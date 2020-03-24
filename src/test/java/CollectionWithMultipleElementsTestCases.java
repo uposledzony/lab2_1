4c54285c899525d0b08944992f6aeebf66232c91
uposledzony <chrustek123456789@gmail.com>
2020-03-24 16:52:14 +0100

    testy_v1 & testy_v2 found mistakes in code that refers previous tasks, forgot to check result of search operation and value that is under estimated position in collection

--- src/test/java/CollectionWithMultipleElementsTestCases.java
+++ src/test/java/CollectionWithMultipleElementsTestCases.java
@@ -1,17 +1,22 @@
 import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static helpers.PositionMatcher.notFound;
+import static helpers.PositionMatcher.positionOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CollectionWithMultipleElementsTestCases {
 
     private int[] collection;
-    private static final int TEST_NUMBER_FIRST =2;
+    private static final int TEST_NUMBER_FIRST = 2;
     private static final int TEST_NUMBER_CENTER = 3;
     private static final int TEST_NUMBER_LAST = 6;
     private static final int ELEMENT_NOT_IN_COLLECTION = 5;
-    private static final int NOT_FOUND = -1;
     private static final int FIRST_POS = 0;
     private static int CENTER_POS;
     private static int LAST_POS;
@@ -24,22 +29,46 @@ public class CollectionWithMultipleElementsTestCases {
     }
 
     @Test
-    void firstElementInProvidedSequenceTest() {
-        assertEquals(FIRST_POS, BinarySearch.create().search(TEST_NUMBER_FIRST, collection).getPosition());
+    void searchFirstElementInProvidedSequenceTest() {
+        SearchResult result = BinarySearch.create().search(TEST_NUMBER_FIRST, collection);
+        int position = result.getPosition();
+        int value = collection[position];
+
+        assertTrue(result.isFound());
+        assertThat(position,is(positionOf(FIRST_POS)));
+        assertThat(value,is(collection[FIRST_POS]));
+
     }
 
     @Test
-    void centerElementInProvidedSequenceTest(){
-        assertEquals(CENTER_POS, BinarySearch.create().search(TEST_NUMBER_CENTER, collection).getPosition());
+    void searchCenterElementInProvidedSequenceTest(){
+        SearchResult result = BinarySearch.create().search(TEST_NUMBER_CENTER, collection);
+        int position = result.getPosition();
+        int value = collection[position];
+
+        assertTrue(result.isFound());
+        assertThat(position,is(positionOf(CENTER_POS)));
+        assertThat(value,is(collection[CENTER_POS]));
     }
 
     @Test
-    void lastElementInProvidedSequenceTest(){
-        assertEquals(LAST_POS, BinarySearch.create().search(TEST_NUMBER_LAST, collection).getPosition());
+    void searchLastElementInProvidedSequenceTest(){
+        SearchResult result = BinarySearch.create().search(TEST_NUMBER_LAST, collection);
+        int position = result.getPosition();
+        int value = collection[position];
+
+        assertTrue(result.isFound());
+        assertThat(position,is(positionOf(LAST_POS)));
+        assertThat(value,is(collection[LAST_POS]));
     }
 
     @Test
-    void noSuchElementInSequenceTest(){
-        assertEquals(NOT_FOUND, BinarySearch.create().search(ELEMENT_NOT_IN_COLLECTION, collection).getPosition());
+    void searchNoSuchElementInSequenceTest(){
+        SearchResult result = BinarySearch.create().search(ELEMENT_NOT_IN_COLLECTION, collection);
+        int position = result.getPosition();
+
+        assertFalse(result.isFound());
+        assertThat(position,is(notFound()));
     }
+
 }
--- src/test/java/TestOneElementCases.java
+++ src/test/java/TestOneElementCases.java
@@ -1,15 +1,22 @@
 import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static helpers.PositionMatcher.notFound;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static helpers.PositionMatcher.positionOf;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestOneElementCases {
+
     private int[] collection;
     private static final int TEST_NUMBER = 6;
     private static final int ELEMENT_NOT_IN_COLLECTION = 5;
-    private static final int NOT_FOUND = -1;
     private static final int POS = 0;
+
     @BeforeEach
     void init(){
         collection = new int[1];
@@ -17,13 +24,23 @@ public class TestOneElementCases {
     }
 
     @Test
-    void elementInProvidedSequenceTest(){
-        assertEquals(POS,BinarySearch.create().search(TEST_NUMBER, collection).getPosition());
+    void searchElementInProvidedSequenceTest(){
+        SearchResult result = BinarySearch.create().search(TEST_NUMBER, collection);
+        int position = result.getPosition();
+        int value = collection[position];
+
+        assertTrue(result.isFound());
+        assertThat(position,is(positionOf(POS)));
+        assertThat(value, is(collection[POS]));
     }
 
     @Test
-    void elementNotInProvidedSequenceTest(){
-        assertEquals(NOT_FOUND, BinarySearch.create().search(ELEMENT_NOT_IN_COLLECTION, collection).getPosition());
+    void searchElementNotInProvidedSequenceTest(){
+        SearchResult result = BinarySearch.create().search(ELEMENT_NOT_IN_COLLECTION, collection);
+        int position = result.getPosition();
+
+        assertFalse(result.isFound());
+        assertThat(position,is(notFound()));
     }
-    
+
 }
