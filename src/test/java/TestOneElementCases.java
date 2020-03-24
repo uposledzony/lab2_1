ccbd6c2534283d23168e9c8ac7f5d12ea789a6aa
uposledzony <chrustek123456789@gmail.com>
2020-03-24 13:18:31 +0100

    testy_v1 added another test case, when provided element is not present in collection during BinarySearch algorithm

--- src/test/java/TestOneElementCases.java
+++ src/test/java/TestOneElementCases.java
@@ -1,4 +1,5 @@
 import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestOneElementCases {
     private int[] collection;
     private static final int TEST_NUMBER = 6;
+    private static final int ELEMENT_NOT_IN_COLLECTION = 5;
+    private static final int NOT_FOUND = -1;
     private static final int POS = 0;
     @BeforeEach
     void init(){
@@ -17,6 +20,11 @@ public class TestOneElementCases {
     @Test
     void elementInProvidedSequenceTest(){
         assertEquals(POS,BinarySearch.create().search(TEST_NUMBER, collection).getPosition());
+    }
+
+    @Test
+    void elementNotInProvidedSequenceTest(){
+        assertEquals(NOT_FOUND, BinarySearch.create().search(ELEMENT_NOT_IN_COLLECTION, collection).getPosition());
     }
     
 }
